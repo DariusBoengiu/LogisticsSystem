9a5a2ae09d906d79f76ac81fc941d478d061caa9
Boengiu <darius.a.boengiu@capgemini.com>
2022-08-02 15:38:50 +0300

    Added Exception Handler

---
+++ src/main/java/com/capgemini/Logistics/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.capgemini.Logistics.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+    private static final long serialVersionUID = 1L;
+    public ResourceNotFoundException(String msg) {
+        super(msg);
+    }
+}
